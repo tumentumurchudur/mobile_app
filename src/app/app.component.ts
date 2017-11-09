d5a5f17c17461c482a401ddf403f2bff2db8b220
James Putnam <jonathan.putnam86@gmail.com>
2017-11-09 16:50:40 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   rootPage:any = 'LoginPage';
 
-
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
