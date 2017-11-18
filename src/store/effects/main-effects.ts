2ef5e8b532b94cb18626d06b416ca160448e4805
AMAIISDOM\ttumurchudur <tumen_23@yahoo.com>
2017-11-18 09:51:02 -0700

    reduces complexity and refactor

--- src/store/effects/main-effects.ts
+++ src/store/effects/main-effects.ts
@@ -9,13 +9,13 @@ import "rxjs/add/operator/switchMap";
 import "rxjs/add/observable/combineLatest";
 import "rxjs/add/observable/fromPromise";
 
-import { LOAD_METERS, LOAD_FROM_DB, LOAD_READS_FROM_DB, AddMeters, LoadFromDb, LoadReadsFromDb } from "../actions";
+import { LOAD_METERS, LOAD_FROM_DB, LOAD_READS_FROM_DB, AddMeters, LoadFromDb } from "../actions";
 import { DatabaseProvider } from "../../providers";
 import { IMeter, IUser, IRates } from "../../interfaces";
-import { StoreServices } from "../../store/services";
 
 import { CostHelper } from "../../helpers";
 import { AddReads } from '../actions/reads-actions';
+import { AddUser, UpdateUser } from '../actions/user-actions';
 
 @Injectable()
 export class MainEffects {
@@ -49,6 +49,7 @@ export class MainEffects {
       if (!meters.length) {
         return new LoadFromDb(user);
       }
+      // Load data from cache.
       return new AddMeters(meters);
     });
 
@@ -64,18 +65,14 @@ export class MainEffects {
     .ofType(LOAD_FROM_DB)
     .map((action: any) => action.payload)
     .switchMap((user: IUser) => {
-
-      // Send request for org path if it is not already in the store.
       return Observable.combineLatest([
-        user.orgPath ? Observable.of(user.orgPath) : this._db.getOrgPathForUser(user.uid),
+        this._db.getOrgPathForUser(user.uid),
         Observable.of(user)
       ]);
     })
     .switchMap((values: any[]) => {
       const [orgPath, user] = values;
-
-      // Update the user in the store once orgPath is available.
-      this._storeServices.updateUser({ orgPath } as IUser);
+      user.orgPath = orgPath;
 
       return Observable.combineLatest([
         this._db.getMetersForOrg(orgPath),
@@ -94,51 +91,12 @@ export class MainEffects {
       const [meters, user] = values;
 
       return Observable.combineLatest([
-        Observable.of(meters),
-        this._storeServices.selectRates().take(1),
+        this._db.getProviderForMeters(meters),
         Observable.of(user)
       ]);
     })
-    .switchMap((values: any[]) => {
-      const [meters, rates, user] = values;
-      let metersWithRates: IMeter[];
-
-      if (rates && rates.length) {
-        // Adds rates data to meters data since it is already available in the store.
-        metersWithRates = meters.map((meter: IMeter) => {
-          const rate = rates.find(r => r._guid === meter._guid);
-
-          return Object.assign({}, meter, {
-            _summer: rate ? rate._summer : null,
-            _winter: rate ? rate._winter : null,
-            _facilityFee: rate ? rate._facilityFee : null
-          });
-        });
-      }
-
-      return Observable.combineLatest([
-        metersWithRates && metersWithRates.length
-          ? Observable.of(metersWithRates)
-          : this._db.getProviderForMeters(meters),
-        Observable.of(rates),
-        Observable.of(user)
-      ]);
-    })
-    .map((values: any[]) => {
-      const [meters, rates, user] = values;
-
-      // Add rates data to the store.
-      if (!rates || !rates.length) {
-        const newRates = meters.map((meter: IMeter) => {
-          return {
-            _guid: meter._guid,
-            _winter: meter._winter,
-            _summer: meter._summer,
-            _facilityFee: meter._facilityFee
-          };
-        });
-        this._storeServices.addRates(newRates);
-      }
+    .flatMap((values: any[]) => {
+      const [meters, user] = values;
 
       // Sets sum of reads diffs to _usage property.
       this._helper.calcUsageDiffs(meters);
@@ -149,12 +107,15 @@ export class MainEffects {
       // Store meter data locally by uid as key.
       this._storage.set(user.uid, meters);
 
-      // Dispatch action to update the store.
-      return new AddMeters(meters);
+      // Dispatch actions to update the store.
+      return [
+        new AddMeters(meters),
+        new UpdateUser(user)
+      ];
     });
 
   @Effect()
-  public loadReadsDataFromDb = this._actions$
+  public refreshReadsDataFromDb = this._actions$
     .ofType(LOAD_READS_FROM_DB)
     .map((action: any) => action.payload)
     .switchMap((meters: IMeter[]) => {
@@ -187,8 +148,7 @@ export class MainEffects {
     private readonly _actions$: Actions,
     private readonly _db: DatabaseProvider,
     private readonly _helper: CostHelper,
-    private readonly _storage: Storage,
-    private readonly _storeServices: StoreServices
+    private readonly _storage: Storage
   ) { }
 
 }
