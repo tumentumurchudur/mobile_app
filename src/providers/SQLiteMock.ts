b1ced74aff0276b785c645a72baca7de9568fba6
MOXL0013\moxtek1 <ttumen@yahoo.com>
2018-02-11 09:57:25 -0700

    Fixes indentation issues

--- src/providers/SQLiteMock.ts
+++ src/providers/SQLiteMock.ts
@@ -1,14 +1,14 @@
-import { SQLite , SQLiteDatabaseConfig } from "@ionic-native/sqlite";
+import { SQLite, SQLiteDatabaseConfig } from "@ionic-native/sqlite";
 
 declare var SQL;
 
 export class SQLiteMock {
   public create(config: SQLiteDatabaseConfig): Promise<SQLiteObject> {
-		return new Promise((resolve, reject) => {
-			var db = new SQL.Database();
+    return new Promise((resolve, reject) => {
+      var db = new SQL.Database();
 
-			resolve(new SQLiteObject(db));
-		});
+      resolve(new SQLiteObject(db));
+    });
   }
 }
 
@@ -16,34 +16,34 @@ class SQLiteObject {
   _objectInstance: any;
 
   constructor(_objectInstance: any) {
-		this._objectInstance = _objectInstance;
+    this._objectInstance = _objectInstance;
   };
 
   executeSql(statement: string, params: any): Promise<any> {
-		return new Promise((resolve, reject) => {
-			try {
-				const st = this._objectInstance.prepare(statement, params);
-				const rows :Array<any> = [];
+    return new Promise((resolve, reject) => {
+      try {
+        const st = this._objectInstance.prepare(statement, params);
+        const rows: Array<any> = [];
 
-				while(st.step()) { 
-					const row = st.getAsObject();
-					rows.push(row);
-				}
+        while (st.step()) {
+          const row = st.getAsObject();
+          rows.push(row);
+        }
 
-				const payload = {
-					rows: {
-						item: function(i) {
-							return rows[i];
-						},
-						length: rows.length
-					},
-					rowsAffected: this._objectInstance.getRowsModified() || 0,
-					insertId: this._objectInstance.insertId || void 0
-				};  
-				resolve(payload);
-			} catch(e){
-				reject(e);
-			}
-		});
+        const payload = {
+          rows: {
+            item: function (i) {
+              return rows[i];
+            },
+            length: rows.length
+          },
+          rowsAffected: this._objectInstance.getRowsModified() || 0,
+          insertId: this._objectInstance.insertId || void 0
+        };
+        resolve(payload);
+      } catch (e) {
+        reject(e);
+      }
+    });
   };
 }
